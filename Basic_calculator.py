cadff7c6a93aa1479fc21273f5d4199d3416e5fb
Khushi Raykhede <raykhede063@gmail.com>
2026-09-01 15:34:51 +0530

    Add files via upload

---
+++ Basic_calculator.py
@@ -0,0 +1,33 @@
+#Simple Calculator
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if b==0:
+        print("Error! division by zero.")
+    return a/b
+ 
+print("Simple Calculator")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice=int(input("Enter your choice(1/2/3/4)"))
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+if choice==1:
+    print("Result:",add(num1,num2))
+elif choice==2:
+    print("Result:",subtract(num1,num2))
+elif choice==3:
+    print("Result:",multiply(num1,num2))
+elif choice==4:
+    print("Result:",divide(num1,num2))
+else:
+    print("Invalid choice!")  
+    
